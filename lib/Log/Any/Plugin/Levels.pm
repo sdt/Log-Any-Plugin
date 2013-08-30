4cfb31ed8641c76d3f5d49e309e60b077df23b7b
Stephen Thirlwall <sdt@dr.com>
2013-08-30 15:06:09 +1000

    Override $log methods directly in Levels plugin

--- lib/Log/Any/Plugin/Levels.pm
+++ lib/Log/Any/Plugin/Levels.pm
@@ -47,11 +47,12 @@ sub install {
     for my $method_name ( Log::Any->logging_methods() ) {
         my $level = $method_name;
 
-        around($adapter_class, $method_name, sub {
-            my ($old_method, $self, @args) = @_;
+        my $old_method = get_old_method($adapter_class, $method_name);
+        set_new_method($adapter_class, $method_name, sub {
+            my $self = shift;
             return if $level_val{ $level_store{$self} || $default_level }
                     > $level_val{$level};
-            $old_method->($self, @args);
+            $self->$old_method(@_);
         });
     }
 
